c11b390787c3625cc0ff1393ebd3eb28d1e39132
Mads Møller Pedersen <42928740+kdl457@users.noreply.github.com>
2020-02-29 15:17:00 +0100

    Add files via upload

--- Simulations/Causal Discovery/varying frequency of H, continuous environments/plot_3_and_comments.R
+++ Simulations/Causal Discovery/varying frequency of H, continuous environments/plot_3_and_comments.R
@@ -7,18 +7,28 @@ res_50 <- readRDS(file = "Simulations/Causal Discovery/Plot3/res_varying_freq_50
 res_50_ell <- readRDS(file = "Simulations/Causal Discovery/Plot3/res_varying_freq_50_ell.RDS")
 res_50_reg <- readRDS(file = "Simulations/Causal Discovery/Plot3/res_varying_freq_50_reg.RDS")
 
-tmp <- data.frame("Accuracy" = c(colSums(res_200)/50, colSums(res_50)/50, 
-                                 colSums(res_50_ell)/50, colSums(res_50_reg)/50), 
+tmp <- data.frame("Accuracy" = c(colSums(res_50)/50, 
+                                 colSums(res_50_ell)/50, colSums(res_50_reg)/50, colSums(res_200)/50), 
                   "Frequency_of_H" = rep(seq(0, 60, length.out = 11), 4), 
-                  "SD" = c(apply(res_200, 2, sd), apply(res_50, 2, sd), 
-                           apply(res_50_ell, 2, sd), apply(res_50_reg, 2, sd)),
-                  "Exp" = c(rep("200_obs", 11), rep("50_obs", 11), rep("50_obs_l1", 11), rep("50_obs_reg", 11)))
-ggplot(data = tmp, aes(x = Frequency_of_H, y = Accuracy, fill = Exp, color = Exp)) + 
-  geom_point(size = 3)  + 
-  ggtitle("Causal Discovery Accuracy with continuous environments") + 
-  xlab("Frequency of H") + 
-  ylab("Accuracy of Causal Discovery") + 
-  theme_minimal() + 
-  theme(plot.title = element_text(hjust = 0.5, size = 22), axis.title = element_text(size = 14)) +
-  geom_errorbar(aes(ymin = Accuracy - SD, ymax=Accuracy + SD), width=.2)
+                  "SD" = c(apply(res_50, 2, sd), apply(res_50_ell, 2, sd), 
+                           apply(res_50_reg, 2, sd), apply(res_200, 2, sd)),
+                  "Method" = c(rep("l_2", 11), rep("l_1", 11), rep("LM", 11), rep("l_2", 11)),
+                  "Setting" = c(rep("50_obs", 33), rep("999_obs", 11)))
 
+
+g <- ggplot(data = tmp, aes(x = factor(tmp$Frequency_of_H), y = Accuracy, color = Method, shape = Setting)) + 
+  geom_errorbar(aes(ymin = Accuracy - SD, ymax=Accuracy + SD), width=.5, position = "dodge") + 
+  geom_point(size = 2, position = position_dodge(width = 0.5))  + 
+  xlab(expression(paste("Frequency of H", ", " ,omega[H]))) + 
+  ylab("Prob. of recov. the causal parent") + 
+  theme_minimal() + 
+  theme(axis.title = element_text(size = 12), 
+        legend.background = element_rect(linetype = 1, size = 0.5, colour = 1)) + 
+  scale_color_discrete(labels = c(expression(paste("l"[1])), expression(paste("l"[2])),"LM" ), 
+                       guide = guide_legend(title.position = "top", title.hjust = 0.5)) + 
+  scale_shape_discrete(labels = c("50", "200"), 
+                       guide = guide_legend(title.position = "top", title.hjust = 0.5))
+g$labels$colour <- "Method"
+g$labels$shape <- "Obs."
+
+ggsave(filename = "Graphics/causal_frequency_H.pdf", plot = g, width = 5.8, height = 3)
