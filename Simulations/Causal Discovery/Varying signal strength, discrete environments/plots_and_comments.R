6eb1f9bb2d1ddfca41ad020e99a9d987551e209f
Mads Møller Pedersen <42928740+kdl457@users.noreply.github.com>
2020-02-29 15:16:37 +0100

    Add files via upload

--- Simulations/Causal Discovery/Varying signal strength, discrete environments/plots_and_comments.R
+++ Simulations/Causal Discovery/Varying signal strength, discrete environments/plots_and_comments.R
@@ -4,16 +4,11 @@ library(ggplot2)
 
 # Discrete environments
 
-res_fixed_200 <- readRDS(file = "Simulations/Causal Discovery/res_matrix_fixed_200.RDS")
-res_fixed_50 <- readRDS(file = "Simulations/Causal Discovery/res_matrix_fixed_50.RDS")
-res_fixed_50_ell_1 <- readRDS(file = "Simulations/Causal Discovery/res_matrix_fixed_50_ell_1.RDS")
-res_fixed_50_10pi <- readRDS(file = "Simulations/Causal Discovery/res_matrix_fixed_50_10pi.RDS")
-res_fixed_reg <- readRDS(file = "Simulations/Causal Discovery/res_matrix_fixed_reg.RDS")
-plot(seq(0.1, 10, length.out = 19), colSums(res_fixed_200)/50, col = "red", ylim = c(0, 1))
-points(seq(0.1, 10, length.out = 19), colSums(res_fixed_50)/50, col = "blue")
-points(seq(0.1, 10, length.out = 19), colSums(res_fixed_50_ell_1)/50, col = "green")
-points(seq(0.1, 10, length.out = 19), colSums(res_fixed_50_10pi)/50, col = "purple")
-# Makes sense since assumption of H varying slower than environments is violated.
+res_fixed_200 <- readRDS(file = "Simulations/Causal Discovery/Plot1/res_matrix_fixed_200.RDS")
+res_fixed_50 <- readRDS(file = "Simulations/Causal Discovery/Plot1/res_matrix_fixed_50.RDS")
+res_fixed_50_ell_1 <- readRDS(file = "Simulations/Causal Discovery/Plot1/res_matrix_fixed_50_ell_1.RDS")
+res_fixed_50_10pi <- readRDS(file = "Simulations/Causal Discovery/Plot1/res_matrix_fixed_50_10pi.RDS")
+res_fixed_reg <- readRDS(file = "Simulations/Causal Discovery/Plot1/res_matrix_fixed_reg.RDS")
 
 tmp_fixed <- data.frame("Accuracy" = c(colSums(res_fixed_200)/50, colSums(res_fixed_50)/50, 
                             colSums(res_fixed_50_ell_1)/50, colSums(res_fixed_50_10pi)/50, colSums(res_fixed_reg)/50),
@@ -21,18 +16,25 @@ tmp_fixed <- data.frame("Accuracy" = c(colSums(res_fixed_200)/50, colSums(res_fi
                   "SD" = c(apply(res_fixed_200, 2, sd), apply(res_fixed_50, 2, sd), 
                            apply(res_fixed_50_ell_1, 2, sd), apply(res_fixed_50_10pi, 2, sd),
                            apply(res_fixed_reg, 2, sd)),
-                  "Exp" = c(rep("200_obs", 19), rep("50_obs", 19), rep("50_obs_l1", 19), rep("50_obs_l1_10pi", 19),
-                            rep("reg", 19)))
+                  "Method" = c(rep("l_2", 38), rep("l_1", 19), rep("l_2", 19), rep("LM", 19)), 
+                  "Setting" = c(rep("999_obs", 19), rep("50_obs", 38), rep("50_obs_10_pi", 19) , rep("50_obs", 19)))
 
-ggplot(data = tmp_fixed, aes(x = Alpha_val, y = Accuracy, fill = Exp, color = Exp)) + 
-  geom_point(size = 3)  + 
-  ggtitle("Causal Discovery Accuracy with fixed coefficients in each environment") + 
-  xlab("Regression coefficient of the hidden variable in Y") + 
-  ylab("Accuracy of Causal Discovery") + 
+g <- ggplot(data = tmp_fixed, aes(x = tmp_fixed$Alpha_val, y = Accuracy, color = Method, shape = Setting)) + 
+  geom_errorbar(aes(ymin = Accuracy - SD, ymax=Accuracy + SD), width=.5, position = "dodge") + 
+  geom_point(size = 2, position = position_dodge(width = 0.5))  + 
+  xlab(expression(paste("Influence of H", ", " ,c[YH2]))) + 
+  ylab("Prob. of recov. the causal parent") + 
   theme_minimal() + 
-  theme(plot.title = element_text(hjust = 0.5, size = 22), axis.title = element_text(size = 14)) +
-  geom_errorbar(aes(ymin = Accuracy - SD, ymax=Accuracy + SD), width=0.2)
+  theme(axis.title = element_text(size = 12), 
+        legend.background = element_rect(linetype = 1, size = 0.5, colour = 1)) + 
+  scale_color_discrete(labels = c(expression(paste("l"[1])),expression(paste("l"[2])), "LM"), 
+                       guide = guide_legend(title.position = "top", title.hjust = 0.5)) +
+  scale_shape_discrete(labels = c(expression(omega[H] == paste(2* pi)), expression(omega[H] == paste(10 * pi)), 
+                                  "200 obs."), 
+                       guide = guide_legend(title.position = "top", title.hjust = 0.5))
+g$labels$colour <- "Method"
+g$labels$shape <- "Setting"
 
-
+ggsave(filename = "Graphics/causal_disc_fixed.pdf", plot = g, width = 5.8, height = 3)
 
 
